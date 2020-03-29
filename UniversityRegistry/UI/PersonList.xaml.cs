222e3bf8408d406bd7021ae4f6f0145b9bad7249
Cole W <cole@willenbrings.com>
2020-03-29 14:29:44 -0500

    WPF Data Binding

--- UniversityRegistry/UI/PersonList.xaml.cs
+++ UniversityRegistry/UI/PersonList.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UniversityRegistry.Data;
 
 namespace UniversityRegistry.UI
 {
@@ -18,9 +20,38 @@ namespace UniversityRegistry.UI
     /// </summary>
     public partial class PersonList : UserControl
     {
+
+        /// <summary>
+        /// A proxy event handler
+        /// </summary>
+        public event SelectionChangedEventHandler SelectionChanged;
+
         public PersonList()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// A proxy event listener that passes on SelectionChanged events
+        /// </summary>
+        /// <param name="sender">The ListView that had its selection changed</param>
+        /// <param name="e">The event arguments</param>
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectionChanged?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Adds a new person to the collection
+        /// </summary>
+        /// <param name="sender">The add person button</param>
+        /// <param name="e"></param>
+        private void OnAddPersonClick(object sender, EventArgs e)
+        {
+            if (DataContext is ObservableCollection<Person> people)
+            {
+                people.Add(new Person() { FirstName = "New", LastName = "Person", DateOfBirth = new DateTime(), Role = Role.UndergraduateStudent, Active = false });
+            }
         }
     }
 }
